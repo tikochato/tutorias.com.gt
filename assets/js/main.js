7cd21a33b5f19cd7c7e2b598d5cf794c1ce43ea7
Tikochato <daor01@gmail.com>
2018-05-21 03:31:13 -0600

    SlideShow de imagenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -202,3 +202,14 @@
 	});
 
 })(jQuery);
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(0)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+}, 3000);
